ccf553ae618769850ca29242ca4ae38443c1f000
Shashank <shashank.shri10@gmail.com>
2024-02-10 18:22:23 +0530

    trial branch

---
+++ src/app/api/textgen/route.js
@@ -0,0 +1,61 @@
+const url = 'https://api.together.xyz/v1/chat/completions';
+const apiKey = '4ed53c5ad29d17d59c9dae03d954057b7a8af5d0d021fa6c312baaa0ea0522e9';
+const apiKey2 = process.env.TAI_KEY;
+const headers = new Headers({
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${apiKey}`
+});
+
+export async function GET(req){
+    try {
+        const { searchParams } = new URL(req.url);
+        const inputText = searchParams.get('inputText');
+        const data = {
+            model: 'openchat/openchat-3.5-1210',
+            max_tokens: 512,
+            top: ['</s>', '[/INST]'],
+            temperature: 0.7,
+            top_p: 0.7,
+            top_k: 50,
+            repetition_penalty: 1,
+            n: 1,
+            messages: [
+                {
+                    role: 'user',
+                    content: inputText
+                }
+            ]
+        };
+        
+        const options = {
+            method: 'POST',
+            headers,
+            body: JSON.stringify(data)
+        };
+        
+        const response = await fetch(url, options);
+        const result = await response.json();
+
+        console.log(result);
+        console.log(result.choices[0].message.content);
+        console.log("input text",inputText);
+        
+        const res = result.choices[0].message.content;
+
+        return {
+            status: response.status,
+            body: JSON.stringify({
+                content: res
+            })
+        };
+    } catch(error) {
+        console.error('Error:', error);
+        console.log(apiKey);
+        return {
+            status: 500, // Internal Server Error
+            body: JSON.stringify({
+                error: error.message
+            })
+        };
+    }
+}
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,7 +1,48 @@
+'use client'    
+import { useState,useEffect } from 'react';
+
 export default function Page(){
+    const [inputText, setInputText] = useState('');
+    const [generatedText, setGeneratedText] = useState('');
+    const [clicked,setclicked] = useState(false);
+    
+    const handleClk = async () => {
+        try {
+            const res = await fetch('')
+            // setGeneratedText(async () => await generateText(inputText))
+            setTimeout(()=>console.log("gen text:",generatedText),5000);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleInputChange = (event) => {
+        setInputText(event.target.value);
+    };
+
     return(
-    <>
-        <h2>Home page</h2>
-        
-    </>
-);}
+        <>
+            <div className="flex flex-col justify-around justify-center items-center h-screen">
+                <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Home page</h2>
+                
+                <input 
+                    type="text" 
+                    className="dark:bg-gray-800 dark:border-gray-600 dark:text-white text-black border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-blue-500 focus:border-blue-500" 
+                    placeholder="Enter Text Here..."
+                    value={inputText}
+                    onChange={handleInputChange}
+                />    
+                <button 
+                    className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-md dark:bg-gray-200 dark:hover:bg-gray-300 dark:text-gray-800"
+                    onClick={handleClk}// Call handleClk when the button is clicked
+                    id="clk-button"
+                >
+                    Generate
+                </button>
+                {generatedText && (
+                    <TextArr inputText={inputText} />
+                )}
+            </div>
+        </>
+    );
+}
