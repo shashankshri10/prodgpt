441a2445d98e909b673962c0105f582eb2cc890a
Shashank <shashank.shri10@gmail.com>
2024-02-12 03:30:18 +0530

    imagen2 rocks baby

---
+++ src/app/api/imagen2/route.js
@@ -0,0 +1,52 @@
+const url = "https://api.together.xyz/v1/completions";
+const apiKey = process.env.TAI_API_KEY;
+
+export async function POST(req2) {
+  const req = await req2.json();
+  const inputText = req.inputText;
+    try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({
+        model: "stabilityai/stable-diffusion-xl-base-1.0",
+        prompt: inputText,
+        negative_prompt: "",
+        request_type: "image-model-inference",
+        width: 1024,
+        height: 1024,
+        steps: 40,
+        n: 4,
+        seed: 9193,
+        promptObj: {
+          prompt: "",
+        },
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const result = await response.json();
+    const stat = response.status;
+    const str = result.choices[0].image_base64;
+    // console.log("Image 64:", str);
+    const responseBody = JSON.stringify({ string: str });
+    const responseHeaders = {
+      "Content-Type": "application/json",
+      // Add more headers if needed
+    };
+
+    return new Response(responseBody, {
+      status: stat,
+      headers: responseHeaders,
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response({ err: error.message });
+  }
+}
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,16 +1,54 @@
 "use client";
 import { useState, useEffect } from "react";
-import { InfinitySpin } from "react-loader-spinner";
+import { InfinitySpin, Triangle } from "react-loader-spinner";
 import "./loading.css";
 
 export default function Page() {
   const [inputText, setInputText] = useState("");
   const [generatedText, setGeneratedText] = useState("");
+  const [imgData, setimgData] = useState("");
   const [clicked, setclicked] = useState(false);
+  const [clicked2, setclicked2] = useState(false);
 
+  const handleClkImage = async () => {
+    setimgData("");
+    setGeneratedText("");
+    setclicked2(true);
+    try {
+      // Define the input text to be sent in the request
+      const it = `logo for a product inspired by ${inputText}`;
+
+      // Make a POST request to the API endpoint
+      const response = await fetch("/api/imagen2/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ inputText: it }),
+      });
+
+      // Parse the JSON response
+      const data = await response.json();
+
+      // Check if the request was successful (status code 200)
+      if (response.ok) {
+        // Handle successful response here
+        // console.log("Response:", data.response);
+        // const decodedImg = atob(data.string);
+        setimgData(data.string);
+      } else {
+        // Handle error response here
+        console.error("Error:", data);
+      }
+    } catch (error) {
+      // Handle network errors or other errors
+      console.error("Error:", error);
+    }
+    handleClk();
+  };
   const handleClk = async () => {
     setclicked(true);
-    setGeneratedText("");
+    // setGeneratedText("");
     try {
       // Define the input text to be sent in the request
       const it = inputText;
@@ -49,41 +87,52 @@ export default function Page() {
 
   return (
     <>
-      <div className="flex flex-col justify-top items-center h-screen">
-        <div
-          className="h-1/4 min-h-1/4 flex flex-col justify-around"
-          style={{ margin: "5vh 0px" }}
-        >
-          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-            Enter Product Idea
-          </h2>
-
-          <input
-            type="text"
-            className="dark:bg-gray-800 dark:border-gray-600 dark:text-white text-black border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-            placeholder="Enter Text Here..."
-            value={inputText}
-            onChange={handleInputChange}
-          />
-          <button
-            className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-md dark:bg-gray-200 dark:hover:bg-gray-300 dark:text-gray-800"
-            onClick={handleClk} // Call handleClk when the button is clicked
-            id="clk-button"
-          >
-            Generate
-          </button>
+      <div className="page-main flex flex-col justify-top items-center h-screen">
+        <h2 className="page-heading text-4xl font-bold tracking-tight text-white sm:text-6xl">
+          Enter Product Idea
+        </h2>
+        <div className=" " style={{ margin: "1vh 0px" }}>
+          <div className="input-prompt-area">
+            {imgData && (
+              <img
+                src={`data:image/jpeg;base64,${imgData}`}
+                style={{ width: "40px", height: "40px" }}
+              />
+            )}
+            <input
+              type="text"
+              className="dark:bg-gray-800  dark:text-white text-black rounded-md py-2 px-4 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Enter Text Here..."
+              value={inputText}
+              onChange={handleInputChange}
+              onKeyDown={(e)=>{if (e.key === 'Enter') handleClkImage();}}
+            />
+            <button onClick={handleClkImage}>Generate</button>
+          </div>
         </div>
+        {!imgData && clicked2 && (
+          <Triangle
+            visible={true}
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="triangle-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        )}
+        {imgData && (
+          <img
+            src={`data:image/jpeg;base64,${imgData}`}
+            style={{ width: "100px", height: "100px" }}
+          />
+        )}
         <div
           className="wrapper mx-10vw white-text overflow-y-scroll"
           //   id="wrapper"
           style={{
             maxHeight: "60vh",
             margin: "0px 10vw",
-            // scrollbarWidth: "thin" /* For Firefox */,
-            // scrollbarColor: "grey black" /* For Firefox */,
-            // WebkitOverflowScrolling: "touch" /* For smooth scrolling on iOS */,
-            // scrollbarTrackColor: "red" /* For IE and Edge */,
-            // scrollbarFaceColor: "red" /* For IE and Edge */,
           }}
         >
           <div>
